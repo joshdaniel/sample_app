4f664850c27c3895df35ffe47dcca9a88af6c223
joshdaniel <josh@joshdaniel.com>
2013-06-30 22:11:03 -0700

    Handle sign-in with incorrect information, show error message.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Sign the user in and redirect to the user's show page.
+      #render 'show'
+    else
+      # Create an error message and re-render the signin form.
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy

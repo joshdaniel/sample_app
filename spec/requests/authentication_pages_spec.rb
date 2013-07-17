6f348bdd23e3fd815f2d5c7ea396fd6d831b5336
joshdaniel <josh@joshdaniel.com>
2013-07-17 13:58:55 -0700

    Exercise 9.3: Add authentication tests that 'profile' and 'settings' do not show up for non-signed in users.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -16,6 +16,9 @@ describe "Authentication" do
 
     let(:submit) { "Sign in" }
 
+    it { should_not have_link('Profile') }
+    it { should_not have_link('Settings') }
+
     describe "with invalid information" do
       before { click_button submit }
 

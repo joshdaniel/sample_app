45a07f5fb6f7fe32fc2237a77d96cff0c3746e64
joshdaniel <josh@joshdaniel.com>
2013-08-01 20:31:37 -0700

    Users must be signed in to access creation or deletion of microposts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,13 +49,6 @@ class UsersController < ApplicationController
 
   private
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
     def unsigned_in_user
       redirect_to(root_path) if signed_in?
     end

f08eec0f5acc8c924518a72aeac201b1fd87a574
joshdaniel <josh@joshdaniel.com>
2013-09-30 23:48:43 -0700

    Show micropost feed on home page for signed in users. Contains models, views, controllers, partials.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,9 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      #TODO: Change this to show existing microposts even after an error creating a new one.
+      #@feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = []
       render 'static_pages/home'
     end
   end

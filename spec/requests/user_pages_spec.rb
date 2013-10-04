d59b8ca9671be52847ff75c4fca28de17011e9b5
joshdaniel <josh@joshdaniel.com>
2013-10-04 00:01:01 -0700

    Revert exercise 10.2 micropost pagination due to flaky tests.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -134,20 +134,6 @@ describe "User pages" do
       it { should have_content(m2.content) }
       it { should have_content(user.microposts.count) }
     end
-
-    describe "micropost pagination" do
-
-      before(:all) { 30.times { FactoryGirl.create(:micropost, user: user) } }
-      after(:all)  {user.microposts.delete_all }
-
-      it { should have_selector('div.pagination') }
-
-      it "should list each micropost for this user" do
-        user.microposts.paginate(page: 1).each do |micropost|
-          page.should have_selector('li', text: micropost.content)
-        end
-      end
-    end
   end
 
   describe "edit" do

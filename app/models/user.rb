4f8f26144bfc816e4c579d40109bd1f18f495406
joshdaniel <josh@joshdaniel.com>
2013-10-30 17:41:10 -0700

    11.1.4: Added followed_users functionality to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
 
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
@@ -34,6 +35,17 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
 
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
   private
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
